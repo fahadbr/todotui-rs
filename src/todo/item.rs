538459205afeff6ead3c370f6bcfc08bf1b2e60f
Fahad Riaz <fahadbr@gmail.com>
2020-08-24 22:47:54 -0400

    small adjustments

--- src/todo/item.rs
+++ src/todo/item.rs
@@ -124,8 +124,12 @@ impl<'a> Item<'a> {
 
         // TODO: finish implementing
         match chars[0] {
-            '@' => self.contexts.push(word.strip_prefix("@").expect("couldn't trip @ symbol")),
-            '+' => self.tags.push(word.strip_prefix("+").expect("couldn't trip + symbol")),
+            '@' => self
+                .contexts
+                .push(word.strip_prefix("@").expect("couldn't trip @ symbol")),
+            '+' => self
+                .tags
+                .push(word.strip_prefix("+").expect("couldn't trip + symbol")),
             _ => {
                 let things: Vec<&str> = word.splitn(2, ":").collect();
                 if things.len() != 2 {
